0dcaed29722e64bf7330193c0c7686ef9c97ce78
Miquel Angel Aloy Vich <40128163+maavcrusoe@users.noreply.github.com>
2021-07-02 15:52:54 +0200

    Update GetKNXValue.ps1
    if you want to insert this values into a SharePoint List only need to use this function https://github.com/maavcrusoe/msgraph365/blob/main/SharePoint/AddItemSharePointList.ps1

--- GetKNXValue.ps1
+++ GetKNXValue.ps1
@@ -1,7 +1,6 @@
 #obtiene todos los datos del KNX
 function GetKNXValue {
     param ( $idgroup )
-
     $user = "user"
     $pass = "pass"
     $Headers = @{ Authorization = "Basic {0}" -f [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user,$pass))) }    
